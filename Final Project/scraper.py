cbb77770ebde4c3462a5304fe72b2fb46b6a69b8
doublebigmak <mak.michael.609@gmail.com>
2019-02-25 12:22:38 -0800

    scraper v0.1 passed: able to retrieve sample statement from FOMC

--- Final Project/scraper.py
+++ Final Project/scraper.py
@@ -3,8 +3,16 @@ import requests
 
 url = 'https://www.federalreserve.gov/newsevents/pressreleases/monetary20190130a.htm'
 
-test_url = 'https://www.michael-mak.ca'
-
 response = requests.get(url, timeout=5)
 
-print(response.content)
+parsed_content = BeautifulSoup(response.content, "html.parser")
+
+text=parsed_content.find('div',attrs={'class':'col-xs-12 col-sm-8 col-md-8'})
+
+#print(text.find_all('p'))
+paragraphs = []
+for i in text.find_all('p'):
+    i=i.text
+    paragraphs.append(i)
+
+print(paragraphs)
