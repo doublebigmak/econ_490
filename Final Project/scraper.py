266d5c9195c1c706136f905020d5bac5c75bf9cb
doublebigmak <mak.michael.609@gmail.com>
2019-02-25 11:56:22 -0800

    used requests library instead of urllib

--- Final Project/scraper.py
+++ Final Project/scraper.py
@@ -1,2 +1,10 @@
-import beautifulsoup4 as bs4
+from bs4 import BeautifulSoup
+import requests
 
+url = 'https://www.federalreserve.gov/newsevents/pressreleases/monetary20190130a.htm'
+
+test_url = 'https://www.michael-mak.ca'
+
+response = requests.get(url, timeout=5)
+
+print(response.content)
