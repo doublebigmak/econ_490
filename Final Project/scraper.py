7926c031c6c7c886428a3d7f11df066a5e18841b
doublebigmak <mak.michael.609@gmail.com>
2019-02-27 00:13:47 -0800

    working on parsing function for OLD pages, currently broked

--- Final Project/scraper.py
+++ Final Project/scraper.py
@@ -5,12 +5,30 @@ url = 'https://www.federalreserve.gov/newsevents/pressreleases/monetary20190130a
 url2='https://www.federalreserve.gov/newsevents/pressreleases/monetary20181108a.htm'
 url_new='https://www.federalreserve.gov/newsevents/pressreleases/monetary20141217a.htm'
 url_oldest_new='https://www.federalreserve.gov/newsevents/pressreleases/monetary20060510a.htm'
+url_old_2 = 'https://www.federalreserve.gov/boarddocs/press/monetary/2004/20040128/default.htm'
+url_oldest = 'https://www.federalreserve.gov/boarddocs/press/general/2000/20000202/'
 
-response = requests.get(url_new, timeout=5)
+response = requests.get(url_oldest, timeout=5)
 
-parsed_content = BeautifulSoup(response.content, "html.parser")
 
-text=parsed_content.find('div',attrs={'class':'col-xs-12 col-sm-8 col-md-8'})
+
+def parse_new(response):
+   
+    parsed_content = BeautifulSoup(response.content, "html.parser")
+
+    text=parsed_content.find('div',attrs={'class':'col-xs-12 col-sm-8 col-md-8'})
+
+    return text
+
+def parse_old(response):
+
+    parsed_content = BeautifulSoup(response.content, "html.parser")
+
+    text=parsed_content.find_all('td','p')
+
+    return text
+
+text = parse_old(response)
 
 #print(text.find_all('p'))
 paragraphs = []
