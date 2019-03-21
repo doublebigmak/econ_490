2faa3a362cd37e721cb317b38277f8f7a93cd824
doublebigmak <mak.michael.609@gmail.com>
2019-03-21 00:06:20 -0700

    continued work on get_dates

--- Final Project/get_dates.py
+++ Final Project/get_dates.py
@@ -16,3 +16,6 @@ new_url = 'https://www.federalreserve.gov/monetarypolicy/fomccalendars.htm'
 year = str(2013) # placeholder year 
 historical_url = 'https://www.federalreserve.gov/monetarypolicy/fomchistorical'+year+'.htm'
 
+response=requests.get(new_url, timeout=10)
+
+parsed_content = BeautifulSoup(response.content, "html.parser")
